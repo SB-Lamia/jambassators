1309df6ce30de39661d040ccaa13a03217d3180b
Zhi Pan <panz5@student.op.ac.nz>
2023-05-27 13:08:48 +1200

    Set up canvas & basic UI button, import textMeshPro

---
+++ Assets/Scripts/TurnsManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnsManager : MonoBehaviour
+{
+    public void EndTurns()
+    {
+        Debug.Log("End Turns");
+        // TODO: Display end-turn animation (lava grows)
+        // Also, disable all player movement, then enable it after the animation is done
+    }
+}

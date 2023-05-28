92556b13bc33b9817cf38f37a363fdb9a97047d5
Lamia <lamialove115@gmail.com>
2023-05-28 12:14:31 +1200

    Player spawn

--- Assets/PopUpTile.cs
+++ Assets/PopUpTile.cs
@@ -6,21 +6,35 @@ using UnityEngine.EventSystems;
 public class PopUpTile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject IconHelp;
+    public bool startGameHelp;
 
     void Awake()
     {
         IconHelp = GameObject.Find(this.gameObject.name + "IconHelp");
-        IconHelp.SetActive(false);
+        startGameHelp = true;
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && startGameHelp == true)
+        {
+            IconHelp.SetActive(false);
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        IconHelp.SetActive(true);
+        if (startGameHelp == false)
+        {
+            IconHelp.SetActive(true);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        IconHelp.SetActive(false);
+        if (startGameHelp == false)
+        {
+            IconHelp.SetActive(false);
+        }
     }
 
 }
